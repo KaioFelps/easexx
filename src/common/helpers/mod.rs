45e078a4656bc8e543292837aee8db1380130cf5
Kaio Felipe <kaiofelipe2005@gmail.com>
2025-05-24 20:53:08 -0300

    feat: add `should_recompile` function

--- src/common/helpers/mod.rs
+++ src/common/helpers/mod.rs
@@ -1,2 +1,4 @@
 pub mod build_source_file;
 pub mod link_all_objects;
+pub mod resolve_output_file;
+pub mod should_recompile;
---
+++ src/common/helpers/should_recompile.rs
@@ -0,0 +1,43 @@
+use std::{fs::File, io::Error, time::SystemTime};
+
+pub struct ShouldRecompileArgs<'a> {
+    pub src_filename: &'a str,
+    pub object_filename: &'a str,
+}
+
+pub fn should_recompile(
+    ShouldRecompileArgs {
+        src_filename,
+        object_filename,
+    }: ShouldRecompileArgs<'_>,
+) -> bool {
+    let src_file = match File::open(src_filename) {
+        Err(_) => return true,
+        Ok(file) => file,
+    };
+
+    let object_file = match File::open(object_filename) {
+        Err(_) => return true,
+        Ok(file) => file,
+    };
+
+    let file_modified_time = match get_modified_time(&src_file) {
+        Err(_) => return true,
+        Ok(time) => time,
+    };
+
+    let obj_file_created_time = match get_created_time(&object_file) {
+        Err(_) => return true,
+        Ok(time) => time,
+    };
+
+    file_modified_time > obj_file_created_time
+}
+
+fn get_modified_time(file: &File) -> Result<SystemTime, Error> {
+    file.metadata()?.modified()
+}
+
+fn get_created_time(file: &File) -> Result<SystemTime, Error> {
+    file.metadata()?.created()
+}
