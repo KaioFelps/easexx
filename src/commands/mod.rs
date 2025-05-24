d3982c71e1e55f8c03ee6b289a68531ea6e02f7c
Kaio Felipe <kaiofelipe2005@gmail.com>
2025-05-24 20:45:02 -0300

    feat: add `EasexxCommand` enum

--- src/commands/mod.rs
+++ src/commands/mod.rs
@@ -1,3 +1,49 @@
+use std::fmt::Display;
+
 pub mod build;
 pub mod man;
 pub mod test;
+
+pub enum EasexxCommand {
+    Man,
+    Build,
+    Test,
+}
+
+impl Display for EasexxCommand {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        write!(
+            f,
+            "{}",
+            match self {
+                Self::Build => "build",
+                Self::Man => "man",
+                Self::Test => "test",
+            }
+        )
+    }
+}
+
+impl TryFrom<&str> for EasexxCommand {
+    type Error = std::io::Error;
+
+    fn try_from(value: &str) -> Result<Self, Self::Error> {
+        match value {
+            "build" => Ok(Self::Build),
+            "test" => Ok(Self::Test),
+            "man" => Ok(Self::Man),
+            _ => Err(std::io::Error::other(
+                "No valid command has been found. Try \"man\" command \
+                for instructions on how to use this CLI.",
+            )),
+        }
+    }
+}
+
+impl TryFrom<String> for EasexxCommand {
+    type Error = std::io::Error;
+
+    fn try_from(value: String) -> Result<Self, Self::Error> {
+        EasexxCommand::try_from(value.as_str())
+    }
+}
